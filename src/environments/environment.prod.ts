b626bc022ab0f79ed7c5623109ebd613779f3d94
Ray19dev <rayhola7@gmail.com>
2025-12-04 00:02:42 -0600

    fix: URL de API corregida y build de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  url_api: 'https://control-escolar-api-iupa.onrender.com'
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@
 
 export const environment = {
   production: false,
-  url_api: "https://control-escolar-api-iupa.onrender.com/"
+  url_api: "https://control-escolar-api-iupa.onrender.com"
 };
